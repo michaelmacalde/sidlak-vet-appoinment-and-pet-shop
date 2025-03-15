0c540a326cfae972c44b3f35ef56726fea15b6b2
rusty0515 <c20222799.decolongon@csav.edu.ph>
2025-03-15 12:14:30 +0800

    update order resource

--- app/Filament/Resources/Ecommerce/OrderResource.php
+++ app/Filament/Resources/Ecommerce/OrderResource.php
@@ -10,13 +10,26 @@ use App\Enums\OrderStatusEnum;
 use App\Models\Ecommerce\Order;
 use App\Enums\PaymentStatusEnum;
 use Filament\Resources\Resource;
+use App\Models\Ecommerce\Product;
+// use Filament\Resources\Pages\Page;
+use App\Ecommerce\Models\OrderItem;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Repeater;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\ToggleButtons;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Resources\UserResource\Pages\EditUser;
+use Filament\Pages\Page;
 use App\Filament\Resources\Ecommerce\OrderResource\Pages;
 use App\Filament\Resources\Ecommerce\OrderResource\RelationManagers;
+
 
 class OrderResource extends Resource
 {
@@ -43,32 +56,176 @@ class OrderResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Select::make('user_id')
-                    ->label('Customer')
-                    ->relationship(name: 'user', titleAttribute: 'name')
-                    ->preload()
-                    ->optionsLimit(5)
-                    ->searchable()
-                    ->getOptionLabelFromRecordUsing(fn ($record) => ucwords($record->name)),
-              
+        ->schema([
+            Section::make('Order Details')
+                ->schema([
+                    Select::make('user_id')
+                        ->label('Customer')
+                        ->relationship(name: 'user', titleAttribute: 'name')
+                        ->preload()
+                        ->optionsLimit(5)
+                        ->required()
+                        ->searchable()
+                        ->getOptionLabelFromRecordUsing(fn ($record) => ucwords($record->name))
+                        ->createOptionForm([
+                            Section::make('User Details')
+                                ->description('The user\'s name and email address.')
+                                ->schema([
+                                    TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255),
 
-                ToggleButtons::make('order_status')
-                    ->options(OrderStatusEnum::class)
-                    ->inline()
-                    ->default(OrderStatusEnum::New)
-                    ->dehydrated()
-                    ->required()
-                    ->label('Order Status'),
+                                    TextInput::make('email')
+                                    ->required()
+                                    ->email()
+                                    ->unique(ignoreRecord: true),
 
-                ToggleButtons::make('payment_status')
-                    ->options(PaymentStatusEnum::class)
-                    ->default(PaymentStatusEnum::Pending)
-                    ->inline()
-                    ->dehydrated()
-                    ->required()
-                    ->label('Payment Status'),
-            ]) ->columns(2);
+                                    TextInput::make('password')
+                                    ->password()
+                                    ->revealable()
+                                    // ->required(fn (Page $livewire): bool => $livewire instanceof EditUser)
+                                    // ->visible(fn (Page $livewire): bool => $livewire instanceof CreateUser)
+                                    ->dehydrateStateUsing(fn ($state) => bcrypt($state)),
+                                    
+
+                                    TextInput::make('password_confirmation')
+                                    ->label('Confirm Password')
+                                    ->password()
+                                    ->same('password')
+                                    // ->required(fn (Page $livewire): bool => $livewire instanceof EditUser)
+                                    // ->visible(fn (Page $livewire): bool => $livewire instanceof CreateUser)
+                                    ->revealable(),
+                                    
+                            ])->columns(2),
+                        ]),
+                       
+    
+                    ToggleButtons::make('order_status')
+                        ->options(OrderStatusEnum::class)
+                        ->default(OrderStatusEnum::New)
+                        ->dehydrated()
+                        ->inline()
+                        ->required()
+                        ->label('Order Status'),
+    
+                    ToggleButtons::make('payment_status')
+                        ->options(PaymentStatusEnum::class)
+                        ->default(PaymentStatusEnum::Pending)
+                        ->inline()
+                        ->dehydrated()
+                        ->required()
+                        ->label('Payment Status'),
+
+
+                   
+                ])
+                ->columns(1),
+
+                Section::make('Addresses')
+                ->schema([
+                    Select::make('shipping_address_id')
+                        ->label('Shipping Address')
+                        ->relationship(name: 'shippingAddress', titleAttribute: 'full_address')
+                        ->preload()
+                        ->required()
+                        ->optionsLimit(5)
+                        ->searchable()
+                        ->createOptionForm([
+                            TextInput::make('street')->required(),
+                            TextInput::make('city')->required(),
+                            TextInput::make('state')->required(),
+                            TextInput::make('zip')->required(),
+                            Hidden::make('address_type')->default('shipping'),
+                        ]),
+
+                    Select::make('billing_address_id')
+                        ->label('Billing Address')
+                        ->relationship(name: 'billingAddress', titleAttribute: 'full_address')
+                        ->preload()
+                        ->required()
+                        ->dehydrated()
+                        ->optionsLimit(5)
+                        ->searchable()
+                        ->createOptionForm([
+                            TextInput::make('street')->required(),
+                            TextInput::make('city')->required(),
+                            TextInput::make('state')->required(),
+                            TextInput::make('zip')->required(),
+                            Hidden::make('address_type')->default('billing'),
+                        ]),
+                        
+                    Toggle::make('is_billing_same_as_shipping')
+                        ->label('Billing same as Shipping')
+                        ->reactive()
+                        ->afterStateUpdated(fn ($state, $set, $get) => 
+                            $state ? $set('billing_address_id', $get('shipping_address_id')) : null
+                        ),
+                ])
+                ->columns(2),       
+
+
+
+    
+            Section::make('Order Items')
+                ->schema([
+                    Repeater::make('orderItems')
+                        ->label('')
+                        ->relationship('orderItems') 
+                        ->schema([
+                            Select::make('product_id')
+                                ->label('Product')
+                                ->relationship(name: 'product', titleAttribute: 'prod_name')
+                                ->preload()
+                                // ->multiple()
+                                ->optionsLimit(5)
+                                ->searchable()
+                                ->required()
+                                ->reactive()
+                                ->afterStateUpdated(fn ($state, $set) => 
+                                    $set('price', Product::find($state)?->prod_price)
+                                   
+                                 )
+                                  ->getOptionLabelFromRecordUsing(fn ($record) => ucwords($record->prod_name)),
+                            
+                            TextInput::make('price')
+                                ->numeric()
+                                ->required()
+                                ->dehydrated()
+                                ->required()
+                                ->label('Price')
+                                ->disabled(),
+                               
+
+                            TextInput::make('quantity')
+                                ->numeric()
+                                ->minValue(1)
+                                ->required()
+                                ->maxValue(fn ($get) => Product::find($get('product_id'))?->prod_quantity ?? 1)
+                                ->reactive()
+                                ->afterStateUpdated(fn ($state, $set, $get) => 
+                                    $set('total', ($get('price')) * ($state))
+                                )
+                                ->label('Quantity'),
+    
+                          
+    
+                            TextInput::make('total')
+                                ->numeric()
+                                ->required()
+                                ->dehydrated()
+                                ->minValue(0)
+                                ->label('Total')
+                                ->disabled(), 
+                              
+                                
+                        ])->columns(2)
+                          //->addable(false)
+                          ->addActionLabel('Add Item')      
+                          ->deletable(fn ($get) => count($get('orderItems')) > 1) 
+                          ->reorderable(),
+                 ]),
+        ]);
+    
             
            
     }
@@ -78,10 +235,36 @@ class OrderResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('user.name')
-                    ->label('Customer')
+                    ->label('Customer Name')
                     ->sortable()
                     ->formatStateUsing(fn (string $state) : string => ucwords($state)) 
                     ->searchable(),
+
+                TextColumn::make('orderItems.product.prod_name')
+                    ->label('Product Ordered')
+                    ->sortable()
+                    ->searchable()
+                    ->formatStateUsing(fn (string $state) : string => ucwords($state)),
+
+                ImageColumn::make('images.url')
+                    ->label('Product Image')
+                    ->circular()
+                    ->height(50)
+                    ->width(50)
+                    ->limit(1)
+                    ->getStateUsing(fn ($record) => 
+                    $record->orderItems->first()?->product->images()
+                        ->where('is_primary', true)
+                        ->value('url')
+                        
+                    ?? 
+
+                    $record->orderItems->first()?->product->images()
+                        ->orderBy('created_at')
+                        ->value('url')
+                ),
+
+
 
                 TextColumn::make('order_status')
                     ->label('Order Status')
